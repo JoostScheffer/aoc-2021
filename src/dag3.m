6c4ebee3b83f86f0bc5f17d9175c8525e153a5be
JScheffer <joostscheffer@protonmail.com>
2021-12-05 22:33:15 +0100

    large optimizations for data conversion

--- src/dag3.m
+++ src/dag3.m
@@ -1,14 +1,14 @@
 fid = fopen('input3.txt', 'rt');
-data = textscan(fid, '%s', 'HeaderLines', 0, 'CollectOutput', true);
+data = textscan(fid, '%c');
 fclose(fid);
 clear fid
 
-array = arrayfun(@str2num, cell2mat(string(data{:})));
+array = reshape((data{:}), [12,1000]).';
 
 % part 1
 gamma = mode(array);
-epsilon = bin2dec(num2str(~(gamma)));
-gamma = bin2dec(num2str(gamma));
+epsilon = bin2dec(char(~(gamma - '0') + '0'));
+gamma = bin2dec(gamma);
 display(gamma*epsilon);
 
 % part 2
@@ -19,17 +19,17 @@ for i = 2:width(array)
     most_common_co2 = mode(co2(:, i));
     most_common_oxygen = mode(oxygen(:, i));
 
-    if and(~all(oxygen(:, i)), height(oxygen ~= 1))
+    if and(~all(oxygen(:, i) - '0'), height(oxygen) ~= 1)
         if height(oxygen(oxygen(:, i) == most_common_oxygen, :)) == (height(oxygen) / 2)
-            most_common_oxygen = 1;
+            most_common_oxygen = '1';
         end
         oxygen = oxygen((oxygen(:, i) == most_common_oxygen), :);
     end
 
 
-    if and(~all(co2(:, i)), height(co2) ~= 1)
+    if and(~all(co2(:, i) - '0'), height(co2) ~= 1)
         if height(co2(co2(:, i) ~= most_common_co2, :)) == (height(co2) / 2)
-            most_common_co2 = 1;
+            most_common_co2 = '1';
         end
         co2 = co2((co2(:, i) ~= most_common_co2), :);
     end
---
+++ src/performance.m
@@ -0,0 +1,11 @@
+N = 5;
+% name = "dag" + string(1:N) + ".m";
+% result = runperf(name);
+% disp(mean(result.TestActivity.MeasuredTime(result.TestActivity.Objective == "sample")));
+disp("day | total time (ms)")
+for i = 1:N
+    name = "dag" + i + ".m";
+    [T, result] = evalc('runperf(name);');
+    disp(i + "   | " + mean(result.TestActivity.MeasuredTime(result.TestActivity.Objective == "sample"))*1e3);
+end
+% arrayfun(@(x) x.MeasuredTime(x.Objective == "sample"), result)
