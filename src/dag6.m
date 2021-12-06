c177353c83ea239ece53eb3c5636b085be3e58ce
JScheffer <joostscheffer@protonmail.com>
2021-12-06 07:38:48 +0100

    conversion optimization

--- src/dag6.m
+++ src/dag6.m
@@ -1,7 +1,7 @@
 data = load("input6.txt");
 
-num = zeros([1,9]);
-for i=0:8
+num = zeros([1, 9]);
+for i = 0:8
     num(i+1) = sum(data == i);
 end
 
@@ -9,7 +9,7 @@ for i = 1:256
     num = circshift(num, -1);
     num(7) = num(7) + num(9);
     if i == 80
-        disp(num2str(sum(num)))
+        fprintf('%d', sum(num))
     end
 end
-disp(num2str(sum(num)))
+fprintf('%d', sum(num))
