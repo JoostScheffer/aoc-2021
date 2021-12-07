8c373ca2bdcef45caffa7a8d4ad5239efcd00fb0
JScheffer <joostscheffer@protonmail.com>
2021-12-07 12:18:47 +0100

    Update dag7.m

--- src/dag7.m
+++ src/dag7.m
@@ -1,5 +1,4 @@
 data = sort(load("input7.txt"));
-% data = sort(load("test7.txt"));
 
 med = data(numel(data)/2);
 fuel1 = sum(abs(data-med));
@@ -8,8 +7,8 @@ disp(fuel1)
 % part 2
 S = data(1);
 L = data(numel(data));
-range = L-S+1;
-dist = abs(repmat(data,range,1) - (S:L).');
-cost = sum(dist.*(dist+1)./2, 2);
+range = L - S + 1;
+dist = abs(repmat(data, range, 1)-(S:L).');
+cost = sum(dist.*(dist + 1)./2, 2);
 [lowest, best] = min(cost);
 disp(lowest)
